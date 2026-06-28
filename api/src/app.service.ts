6744e2dd5f21fd4a052ab091df7090fdaf9d1876
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-06-28 23:55:22 +0500

    Fixed AppService

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs/promises';
+
+export interface Bookmark {  
+    id: string;
+    title: string;
+    url: string;
+    description: string;
+    folders: string;
+    tags: string;
+};
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello, Nest!';
-  }
+  
+    async getAllDataOfBookmarks() : Promise<Bookmark[]> { 
+        const filePath = path.join(process.cwd() ,'data', 'bookmarks.json');
+
+        try { 
+            const fileContent = await fs.readFile(filePath, 'utf-8');
+            return JSON.parse(fileContent);
+        } catch(error) {
+            throw new Error("Не удалось прочитать файл");
+        }
+        };
+
+    async createNewBookmark() : Promise<Bookmark[]> {
+        const booksmarks = await this.getAllDataOfBookmarks();
+    }
 }
