d80443e0feaaf024ce16a2479b99a2156e62c7d2
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-06-30 02:24:18 +0500

    Fixed app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { FolderModule } from './folders/folders.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    BookmarksModule,
+    FolderModule,
+    TagsModule,
+  ],
+  exports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
