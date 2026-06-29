5b0950957b23e208dd62fa12b6b6dbcdc5cf0aa5
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-06-30 02:23:54 +0500

    Fixed app controller and service

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,23 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { AppService } from './app.service';
-import type { Bookmark } from './app.service';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
 
-	@Get('/bookmarks')
-		getData() {
-			return this.appService.getAllDataOfBookmarks();
-		}
-
-	@Post('/createbookmark')
-		postData(@Body() newBookmark: Bookmark) {
-			return this.appService.createNewBookmark(newBookmark);
-		}
-
-	@Delete('bookmarks/:id')
-		deleteData(@Param('id') id: string) {
-			return this.appService.deleteSelectedBookmark(id);
-		}
 }
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,56 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import * as path from 'path';
-import * as fs from 'fs/promises';
-
-export interface Bookmark {  
-    id: string;
-    title: string;
-    url: string;
-    description: string;
-    folders: string;
-    tags: string;
-};
 
 @Injectable()
 export class AppService {
-   
-    private filePath = path.join(process.cwd() ,'data', 'bookmarks.json');
 
-    async getAllDataOfBookmarks() : Promise<Bookmark[]> { 
-
-        try { 
-            const fileContent = await fs.readFile(this.filePath, 'utf-8');
-            return JSON.parse(fileContent);
-        } catch(error) {
-            if(this.filePath.length === 0) {
-                return [];
-            }
-            throw new Error("GET: Не удалось прочитать файл");
-        }
-    };
-
-    async createNewBookmark(newBookmark: Bookmark) : Promise<Bookmark[]> {
-        const bookmarks = await this.getAllDataOfBookmarks();
-        bookmarks.push(newBookmark);
-
-        try {
-            const stringifiedData = JSON.stringify(bookmarks)
-            await fs.writeFile(this.filePath, stringifiedData, 'utf-8');
-            return bookmarks;
-
-        } catch(error) {
-            throw new Error("POST: не удалось создать новую закладку")
-        }
-    };
-
-    async deleteSelectedBookmark(id: string) : Promise<void> {
-        try {
-            const bookmarks = await this.getAllDataOfBookmarks();
-            const filteredBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
-            const stringifiedData = JSON.stringify(filteredBookmarks);
-            await fs.writeFile(this.filePath, stringifiedData, 'utf-8');
-        } catch(error) {
-            throw new Error("DELETE: не удалось выполнить удаление выбранного элемента");
-        }
-    }
 }
